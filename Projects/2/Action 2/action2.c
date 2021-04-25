5b7784cb6262a9f9fccac636d6ca563b4f72e565
RobKulesa <robjkulesa@gmail.com>
2021-04-25 17:28:00 -0400

    Fix action 2, upload results for action 1

--- Projects/2/Action 2/action2.c
+++ Projects/2/Action 2/action2.c
@@ -5,19 +5,176 @@
 #include <pthread.h>
 #include <time.h>
 
-void *sortsubarray(void *arr);
+void *sorterthread(void *args);
+void *reptrhead(void *args);
 void merge(int arr[], int l, int m, int r);
 void mergeSort(int arr[], int l, int r);
 
-typedef struct subarray {
+typedef struct sorterarray {
     int* arr;
     int length;
-    int groupnum;
-    int threadnum;
-    int grouplength;
-    int* groupbase;
+    char* path;
+}sorterarray;
+
+typedef struct reparray {
     int num_threads_per_group;
-}subarray;
+    int group_chunk_length;
+    int groupnum;
+    int num_groups;
+    int group_remainder;
+    int* arr;
+}reparray;
+
+pthread_mutex_t* mutex;
+
+void *sorterthread(void *args) {
+    sorterarray* sorterarr = (sorterarray*) args;
+    int* arr = sorterarr->arr;
+    int length = sorterarr->length;
+    char* path = sorterarr->path;
+
+    FILE* groupfile = fopen(path, "a+");
+
+    mergeSort(arr, 0, length - 1);
+
+    pthread_mutex_lock(mutex);
+    for(int i = 0; i < length; ++i) {
+        fprintf(groupfile, "%d,", arr[i]);
+    }
+    pthread_mutex_unlock(mutex);
+    fclose(groupfile);
+    return (void*) 0;
+}
+
+void *repthread(void *args) {
+    reparray* reparr = (reparray*) args;
+    int num_threads_per_group = reparr->num_threads_per_group;
+    int group_chunk_length = reparr->group_chunk_length;
+    int groupnum = reparr->groupnum;
+    int num_groups = reparr->num_groups;
+    int group_remainder = reparr->group_remainder;
+    int* arr = reparr->arr;
+    pthread_t sorters[num_threads_per_group];
+    char path[17];
+    sprintf(path, "./tmp/group%d.txt", groupnum+1);
+    /* printf("Path: %s\n", path); */
+
+    int thread_chunk_length = group_chunk_length / num_threads_per_group;
+    int thread_remainder = group_chunk_length - thread_chunk_length * num_threads_per_group;
+
+    if(groupnum == num_groups - 1) {
+        thread_chunk_length = (group_chunk_length + group_remainder) / num_threads_per_group;
+        thread_remainder = (group_chunk_length + group_remainder) - thread_chunk_length * num_threads_per_group;
+    }
+    sorterarray* sorterarrs[num_threads_per_group];
+    for(int j = 0; j < num_threads_per_group; j++) {
+        int start = j*thread_chunk_length;
+        int threadarrlength;
+        if(j == num_threads_per_group - 1) {
+            threadarrlength = thread_chunk_length + thread_remainder;
+        } else {
+                threadarrlength = thread_chunk_length;
+        }
+        sorterarray* sorterarr = (sorterarray*) calloc(1, sizeof(sorterarray));
+        sorterarr->arr = (int*) &arr[start];
+        sorterarr->length = threadarrlength;
+        sorterarr->path = path;
+        sorterarrs[j] = sorterarr;
+    }
+
+    FILE* groupfile = fopen(path, "w+");
+    fclose(groupfile);
+
+    int length = 0;
+    for(int i = 0; i < num_threads_per_group; ++i) {
+        length += sorterarrs[i]->length;
+        pthread_create(&sorters[i], NULL, sorterthread, sorterarrs[i]);
+    }
+    
+    for(int i = 0; i < num_threads_per_group; ++i) {
+        pthread_join(sorters[i], NULL);
+    }
+    groupfile = fopen(path, "r");
+    int* totalarr = calloc(length, sizeof(int));
+    for(int i = 0; i < length; ++i) {
+        fscanf(groupfile, "%d,", &totalarr[i]);
+    }
+    fclose(groupfile);
+    mergeSort(totalarr, 0, length - 1);
+    if(length <= 100) {
+        printf("Group Sorted Arr: ");
+        for(int i = 0; i < length; ++i) {
+            printf("%d ", totalarr[i]);
+        }
+        printf("\n");
+    }
+    return (void*) 0;
+}
+
+int main(int argc, char** argv) {
+    srand(getpid());
+    mutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+    int list_length;
+    printf("Enter length of integer list (O(1k) - O(1m)): ");
+    scanf("%d", &list_length);
+
+    int numArray[list_length];
+    for(int i = 0; i < list_length; ++i) {
+        numArray[i] = rand() % 10000000;
+    }
+
+    /* printf("Arr is: ");
+    for(int i = 0; i < list_length; ++i) {
+        printf("%d ", numArray[i]);
+    }
+    printf("\n"); */
+
+    int num_groups;
+    printf("Enter number of thread groups (rec. 4): ");
+    scanf("%d", &num_groups);
+
+    int num_threads_per_group;
+    printf("Enter number of threads per group: (try 4, 100, 1000): ");
+    scanf("%d", &num_threads_per_group);
+
+
+    pthread_t groups[num_groups];
+    reparray* reparrs[num_groups];
+    clock_t groupTimers[num_groups];
+
+    int group_chunk_length = list_length / num_groups;
+    int group_remainder = list_length - group_chunk_length * num_groups;
+    for(int i = 0; i < num_groups; ++i) {
+        int start = i*group_chunk_length;
+        reparray* reparr = (reparray*) calloc(1, sizeof(reparray)); 
+        
+        
+        reparr->groupnum = i;
+        reparr->num_groups = num_groups;
+        reparr->group_remainder = group_remainder;
+        reparr->group_chunk_length = group_chunk_length;
+        reparr->arr = (int*) calloc(group_chunk_length, sizeof(int));
+        reparr->arr = &numArray[start];
+        reparr->num_threads_per_group = num_threads_per_group;
+        reparrs[i] = reparr;
+        groupTimers[i] = clock();
+        pthread_create(&groups[i], NULL, repthread, reparrs[i]);
+    }
+
+    for(int i = 0; i < num_groups; ++i) {
+        pthread_join(groups[i], NULL);
+    }
+
+    for(int i = 0; i < num_groups; ++i) {
+        for(int j = 0; j < num_threads_per_group; j++) {
+            if(j==num_threads_per_group-1) { 
+                groupTimers[i] = clock() - groupTimers[i]; 
+                printf("Group %d took %.3f ms!\n", i+1, ((double) groupTimers[i])/CLOCKS_PER_SEC*100);
+            }
+        }
+        free(reparrs[i]);
+    }
+}
 
 void merge(int arr[], int l, int m, int r) {
     int i, j, k;
@@ -68,131 +225,3 @@ void mergeSort(int arr[], int l, int r) {
         merge(arr, l, m, r);
     }
 }
-void *sortsubarray(void *arr) {
-    subarray* threadarray = (subarray*) arr;
-
-    int* numArray = threadarray->arr;
-    int length = threadarray->length;
-    int groupnum = threadarray->groupnum;
-    int threadnum = threadarray->threadnum;
-    int grouplength = threadarray->grouplength;
-    int* groupbase = threadarray->groupbase;
-    int num_threads_per_group = threadarray->num_threads_per_group;
-    /* for(int i = 0; i < length; ++i) {
-        printf("Thread %d: %d\n", (int) pthread_self(), numArray[i]);
-    } */
-
-    /* printf("Thread %d Before Sorted: ", (int) pthread_self());
-    for(int i = 0; i < length; ++i) {
-        printf("%d ", numArray[i]);
-    }
-    printf("\n"); */
-
-    mergeSort(numArray, 0, length - 1);
-
-    /* printf("Thread %d After Sorted: ", (int) pthread_self());
-    for(int i = 0; i < length; ++i) {
-        printf("%d ", numArray[i]);
-    }
-    printf("\n"); */
-
-    if(threadnum == num_threads_per_group - 1) {
-        int* totalArray = (int*) calloc(grouplength, sizeof(int));
-        memcpy(totalArray, groupbase, grouplength*sizeof(int));
-        mergeSort(totalArray, 0, grouplength - 1);
-        printf("Group %d Rep Thread %d Finished Sorting!\n", groupnum+1, threadnum+1);
-
-        if(grouplength <= 100) {
-            printf("Group %d Sorted Arr: ", groupnum+1);
-            for(int i = 0; i < grouplength; ++i) {
-                printf("%d ", totalArray[i]);
-            }
-            printf("\n");
-        }
-
-        free(totalArray);
-    }
-
-    
-
-    return (void*) 0;
-}
- 
-
-int main(int argc, char** argv) {
-    int list_length;
-    printf("Enter length of integer list (O(1k) - O(1m)): ");
-    scanf("%d", &list_length);
-
-    int numArray[list_length];
-    for(int i = 0; i < list_length; ++i) {
-        numArray[i] = rand() % 10000;
-    }
-
-    int num_groups;
-    printf("Enter number of thread groups (rec. 5): ");
-    scanf("%d", &num_groups);
-
-    int num_threads_per_group;
-    printf("Enter number of threads per group: (try 4, 100, 1000): ");
-    scanf("%d", &num_threads_per_group);
-
-    /* printf("Arr is: ");
-    for(int i = 0; i < list_length; ++i) {
-        printf("%d ", numArray[i]);
-    }
-    printf("\n"); */
-
-
-    pthread_t groups[num_groups][num_threads_per_group];
-    subarray* threadarrs[num_groups][num_threads_per_group];
-    clock_t groupTimers[num_groups];
-
-    int group_chunk_length = list_length / num_groups;
-    int group_remainder = list_length - group_chunk_length * num_groups;
-    for(int i = 0; i < num_groups; ++i) {
-        int thread_chunk_length = group_chunk_length / num_threads_per_group;
-        int thread_remainder = group_chunk_length - thread_chunk_length * num_threads_per_group;
-
-        if(i == num_groups - 1) {
-            thread_chunk_length = (group_chunk_length + group_remainder) / num_threads_per_group;
-            thread_remainder = (group_chunk_length + group_remainder) - thread_chunk_length * num_threads_per_group;
-        }
-
-        for(int j = 0; j < num_threads_per_group; j++) {
-            int start = i*group_chunk_length + j*thread_chunk_length;
-            int threadarrlength;
-            subarray *threadarr = (subarray*) calloc(1, sizeof(subarray));
-            threadarrs[i][j]=threadarr;
-
-            if(j == num_threads_per_group - 1) {
-                threadarrlength = thread_chunk_length + thread_remainder;
-            } else {
-                threadarrlength = thread_chunk_length;
-            }
-
-            threadarr->arr = (int*) &numArray[start];
-            threadarr->length = threadarrlength;
-            threadarr->groupnum = i;
-            threadarr->threadnum = j;
-            threadarr->groupbase = (int*) &numArray[i*group_chunk_length];
-            threadarr->grouplength = thread_chunk_length*num_threads_per_group+thread_remainder;
-            threadarr->num_threads_per_group = num_threads_per_group;
-            if(j==0) groupTimers[i] = clock();
-            pthread_create(&groups[i][j], NULL, sortsubarray, threadarr);
-        }
-    }
-
-    for(int i = 0; i < num_groups; ++i) {
-        for(int j = 0; j < num_threads_per_group; j++) {
-            pthread_join(groups[i][j], NULL);
-            if(j==num_threads_per_group-1) { 
-                groupTimers[i] = clock() - groupTimers[i]; 
-                printf("Group %d took %.3f ms!\n", i+1, ((double) groupTimers[i])/CLOCKS_PER_SEC*100);
-            }
-            subarray *threadarr = threadarrs[i][j];
-            free(threadarr);
-        }
-    }
-}
-
